6260ccd946952425972fe61b2610c2fd1930ede0
zhaohejing <454258314@qq.com>
2018-10-04 13:08:12 +0800

    fegin 客户端添加

---
+++ application/authcustomer/src/test/java/com/monkey/authcustomer/AuthcustomerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.monkey.authcustomer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthcustomerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
