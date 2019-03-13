4a48780c5ed0f194a6d9d825db6786386019e958
zhaohejing <zhaohejing@beisenyun.com>
2019-03-13 18:33:03 +0800

    添加动态数据源

---
+++ tenant-starter/src/main/java/com/monkey/tenant/DynamicDatasourceConfigProperties.java
@@ -0,0 +1,36 @@
+package com.monkey.tenant;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "dynamicds")
+public class DynamicDatasourceConfigProperties {
+    private String orgCodeHeader;
+    private Map<String, String> general;
+    private Map<String, Map<String, String>> tenants;
+
+    public String getOrgCodeHeader() {
+        return orgCodeHeader;
+    }
+
+    public void setOrgCodeHeader(String orgCodeHeader) {
+        this.orgCodeHeader = orgCodeHeader;
+    }
+
+    public Map<String, String> getGeneral() {
+        return general;
+    }
+
+    public void setGeneral(Map<String, String> general) {
+        this.general = general;
+    }
+
+    public Map<String, Map<String, String>> getTenants() {
+        return tenants;
+    }
+
+    public void setTenants(Map<String, Map<String, String>> tenants) {
+        this.tenants = tenants;
+    }
+}
