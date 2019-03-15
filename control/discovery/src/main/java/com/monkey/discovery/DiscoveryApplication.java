d37af1a6750eecc6cb1a5aa0800c1bc1431910b0
zhaohejing <zhaohejing@beisenyun.com>
2019-03-15 17:08:51 +0800

    更换配置中心

--- control/discovery/src/main/java/com/monkey/discovery/DiscoveryApplication.java
+++ control/discovery/src/main/java/com/monkey/discovery/DiscoveryApplication.java
@@ -1,12 +1,19 @@
 package com.monkey.discovery;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication()
 @EnableEurekaServer
+@EnableApolloConfig
 public class DiscoveryApplication {
+//    static {
+//        System.setProperty("-Dapp.id","ghost");
+//        System.setProperty("-Denv","dev");
+//        System.setProperty("-Ddev_meta","http://47.94.214.199:8080");
+//    }
     public static void main(String[] args) {
         SpringApplication.run( DiscoveryApplication.class, args );
     }
