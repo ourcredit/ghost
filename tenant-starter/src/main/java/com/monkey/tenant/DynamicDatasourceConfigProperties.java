656626e41ae52166af2a66bfee5dda894e4fe6d9
zhaohejing <zhaohejing@beisenyun.com>
2019-03-19 13:58:38 +0800

    远程配置

--- tenant-starter/src/main/java/com/monkey/tenant/DynamicDatasourceConfigProperties.java
+++ tenant-starter/src/main/java/com/monkey/tenant/DynamicDatasourceConfigProperties.java
@@ -1,14 +1,19 @@
 package com.monkey.tenant;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.Map;
 @ConfigurationProperties(prefix = "dynamicds")
 public class DynamicDatasourceConfigProperties {
+    @ApolloConfig("tenants.yml")
+    private Config _config;
+
     private String orgCodeHeader;
     private Map<String, String> general;
     private Map<String, Map<String, String>> tenants;
     public Map<String, String> getGeneral() {
-        return general;
+        return  general;
     }
     public void setGeneral(Map<String, String> general) {
         this.general = general;
@@ -24,7 +29,7 @@ public class DynamicDatasourceConfigProperties {
 
 
     public String getOrgCodeHeader() {
-        return orgCodeHeader;
+        return _config.getProperty("dynamicds.orgCodeHeader","TenantId");
     }
 
     public void setOrgCodeHeader(String orgCodeHeader) {
