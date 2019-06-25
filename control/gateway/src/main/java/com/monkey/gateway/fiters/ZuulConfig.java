b81f195ddf0cd47b3f252a62526efd48054480b2
zhaohejing <454258314@qq.com>
2019-06-25 21:15:42 +0800

    配置修改

--- control/gateway/src/main/java/com/monkey/gateway/fiters/ZuulConfig.java
+++ control/gateway/src/main/java/com/monkey/gateway/fiters/ZuulConfig.java
@@ -2,20 +2,14 @@ package com.monkey.gateway.fiters;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
-@EnableApolloConfig("zuulconfig")
+@EnableApolloConfig
 public class ZuulConfig {
-    @Bean(name = "zuul.configuration.properties")
-    @RefreshScope
-    @Primary
+    @ConfigurationProperties("zuul")
     public ZuulProperties zuulProperties(){
-        ZuulProperties p=  new ZuulProperties();
-        return  p;
+        return new ZuulProperties();
     }
 }
