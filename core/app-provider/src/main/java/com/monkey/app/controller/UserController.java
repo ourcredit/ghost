2b4d533c75e9ef505ce48d4da8fdaf20be0b14b7
zhaohejing <454258314@qq.com>
2019-07-03 12:02:20 +0800

    gateway 跨域

--- core/app-provider/src/main/java/com/monkey/app/controller/UserController.java
+++ core/app-provider/src/main/java/com/monkey/app/controller/UserController.java
@@ -16,6 +16,7 @@ import result.LoginInput;
 import result.Result;
 import tools.JWTUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,16 +33,17 @@ import java.util.Map;
 public class UserController {
     @Autowired
     IIMUserService _userService;
-
-    @RequestMapping(value = "", method = RequestMethod.POST)
+    /*用户列表*/
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
     // @RequiresPermissions(value = {PermissionConst._devices._device.list})
-    public Result<IPage<IMUser>> users(@RequestBody PageFilterInputDto page, @CurrentUser IMUser user) throws Exception {
+    public Result<IPage<IMUser>> users(@RequestBody PageFilterInputDto page) throws Exception {
         Wrapper filter = WrapperUtil.toWrapper(page);
         IPage<IMUser> res = _userService.page(WrapperUtil.toPage(page), filter);
         return new Result<>(RequestConstant.SUCCESSCODE, RequestConstant.SUCCESSMSG, res);
     }
-    @RequestMapping(value = "/testlogin", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public Result<Object> checkLogin(@RequestBody LoginInput input) {
+    /*后台登陆接口*/
+    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public Result<Object> checkLogin(HttpServletRequest req, @RequestBody LoginInput input) {
         ApiResult returnResult = new ApiResult();
         Map<String, Object> returnData = new HashMap<>();
         ServerInfoEntity serverinfo = new ServerInfoEntity();
@@ -62,6 +64,7 @@ public class UserController {
         returnUsers.remove("password");
 
         returnData.put("token", JWTUtil.sign(users.getUsername(), users.getId(), users.getAppId(), users.getSalt()));
+        returnData.put("ipAddress", JWTUtil.getIpAddress(req));
         returnData.put("userinfo", returnUsers);
         returnData.put("serverinfo", serverinfo);
         returnData.put("bqmmplugin", bmqq_plugin);
@@ -69,6 +72,30 @@ public class UserController {
         returnResult.setCode(ApiResult.SUCCESS);
         returnResult.setData(returnData);
         returnResult.setMessage("登录成功!");
+        return new Result<Object>(1,RequestConstant.SUCCESSMSG,returnResult);
+    }
+    /*获取当前用户信息*/
+    @RequestMapping(value = "/current", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public Result<Object> checkLogin(HttpServletRequest req,@CurrentUser IMUser user) {
+        ApiResult returnResult = new ApiResult();
+        Map<String, Object> returnData = new HashMap<>();
+        ServerInfoEntity serverinfo = new ServerInfoEntity();
+        Map<String, Object> bmqq_plugin = new HashMap<>();
+        IMUser users = _userService.getById(user.getId());
+        if (users == null || users.getId() == 0) {
+            return  Result.NotFound();
+        }
+        Map<String, Object> returnUsers = JavaBeanUtil.convertBeanToMap(users);
+        returnUsers.put("peerId", users.getId());
+        returnUsers.remove("password");
+        returnData.put("ipAddress", JWTUtil.getIpAddress(req));
+        returnData.put("userinfo", returnUsers);
+        returnData.put("serverinfo", serverinfo);
+        returnData.put("bqmmplugin", bmqq_plugin);
+        _userService.updateById(users);
+        returnResult.setCode(ApiResult.SUCCESS);
+        returnResult.setData(returnData);
+        returnResult.setMessage("获取信息成功!");
         return new Result<Object>(1,RequestConstant.SUCCESSMSG,returnResult);
     }
 
