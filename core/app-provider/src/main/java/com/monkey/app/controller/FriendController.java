1e68477a93133ede3cc7b05c1a948853cc2a174a
zhaohejing <454258314@qq.com>
2019-07-25 22:00:25 +0800

    新增接口

---
+++ core/app-provider/src/main/java/com/monkey/app/controller/FriendController.java
@@ -0,0 +1,18 @@
+package com.monkey.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author cloudtalk
+ * @since 2019-01-04
+ */
+@RestController
+@RequestMapping(value = "/api/record")
+public class RecordController {
+        
+}
